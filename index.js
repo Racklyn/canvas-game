d88fee4c012e2e81bfcd932594bf29a633855a70
Racklyn <racklynsotero@gmail.com>
2024-03-20 20:07:58 -0300

    Add game character left and right motion

--- index.js
+++ index.js
@@ -9,36 +9,50 @@ const chaoAltura = 150
 // Valores do personagem:
 const pLargura = 20
 const pAltura = 50
+const passo = 5
 let y = altura - chaoAltura - pAltura
 let x = 200
 
+function desenhar() {
+    // Pintando o céu
+    ctx.fillStyle = "#3DF"
+    ctx.fillRect(0, 0, largura, altura)
+
+    // Pintando o chão
+    ctx.fillStyle = "#1D6"
+    ctx.fillRect(0, altura - chaoAltura, largura, altura)
+
+    // Desenhando personagem
+    ctx.fillStyle = "#F20"
+    ctx.fillRect(x, y, pLargura, pAltura)
+}
 
 
-// Pintando o céu
-ctx.fillStyle = "#3DF"
-ctx.fillRect(0, 0, largura, altura)
-
-// Pintando o chão
-ctx.fillStyle = "#1D6"
-ctx.fillRect(0, altura - chaoAltura, largura, altura)
-
-// Desenhando personagem
-ctx.fillStyle = "#F20"
-ctx.fillRect(x, y, pLargura, pAltura)
 
 
 
+// EXECUÇÕES ######################################
 
-// document.addEventListener("keydown", e => {
-    
-//     if (e.key == "ArrowRight") {
-//         x += 10
-//     }else if (e.key == "ArrowLeft") {
-//         x -= 10
-//     }
-//     console.log(x)
 
-//     // Desenhando personagem
-//     ctx.fillStyle = "#F20"
-//     ctx.fillRect(x, y, pLargura, pAltura)
-// })
+desenhar()
+
+
+document.addEventListener("keydown", (event) => {
+    console.log(event.key)
+
+
+    if(event.key == "ArrowRight"){
+        console.log("Para a direita --->")
+        x = x + passo
+        console.log("O valor de X é: " + x)
+        desenhar()
+    }
+ 
+    if(event.key == "ArrowLeft"){
+        console.log("Para a esquerda <---")
+        x = x - passo
+        console.log("O valor de X é: " + x)
+        desenhar()
+    }       
+
+})
